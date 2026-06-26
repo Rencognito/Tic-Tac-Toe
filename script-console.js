44694f182448d65c71cefcd67b0e0c35a0fb8928
erengorgun-nkit <115351981+erengorgun-nkit@users.noreply.github.com>
2026-06-26 12:43:15 +0300

    console user input and win check

--- script-console.js
+++ script-console.js
@@ -1,3 +1,27 @@
+import { createInterface } from "node:readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function takeInput(input) {
+  return new Promise((resolve) => {
+    rl.question(input, (answer) => {
+      resolve(answer);
+    });
+  });
+}
+
+async function createX(name, marker, moveList, score) {
+  name = await takeInput(`Enter player (${marker}) name: `);
+  return { name, marker, moveList, score };
+}
+async function createO(name, marker, moveList, score) {
+  name = await takeInput(`Enter player (${marker}) name: `);
+  return { name, marker, moveList, score };
+}
+
 const gameboard = (() => {
   const board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const getBoard = () => board;
@@ -21,9 +45,6 @@ const winConditions = [
   [3, 5, 7]
 ];
 
-function createPlayer(name, marker, moves) {
-  return { name, marker, moves };
-}
 
 function printBoard(b) {
   console.log(`${b[0]}|${b[1]}|${b[2]}
@@ -31,20 +52,37 @@ ${b[3]}|${b[4]}|${b[5]}
 ${b[6]}|${b[7]}|${b[8]}`);  
 }
 
-const rl = require("node:readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
-function playerTurn(player) {
-  printBoard(gameboard.getBoard());
-  rl.question(`${player.name} (${player.marker}) choose cell: `, move => {
-    if (Number(move) != NaN) {
-      gameboard.placeMarker(move, player.marker);
-      console.log("\n-----\n");
-      rl.close();
+function checkWin(player) {
+  const moves = player.moveList.toString();
+  for (let i = 0; i < winConditions.length; i++) {
+    if (moves.includes(i.toString())) {
+      playerX.score++;
+      console.log(`${playerX.name} wins!`);
+      printBoard(gameboard.getBoard());
+      return true;
+      break;
     }
-  });
+    else {
+      return false;
+      break;
+    }
+  }
 }
 
-playerTurn(createPlayer("p1", "X"));
+async function playerTurn(player) {
+  printBoard(gameboard.getBoard());
+  const move = await takeInput(`${player.name} (${player.marker}) choose cell: `);
+  if (Number(move) != NaN) {
+    gameboard.placeMarker(move, player.marker);
+    player.moveList.push(Number(move));
+    console.log("\n-----\n");
+  }
+  checkWin(player);
+}
+
+async function newGame() {
+  const playerX = await createX(undefined, "X", [], 0);
+  const playerO = await createX(undefined, "O", [], 0);
+}
+newGame();
