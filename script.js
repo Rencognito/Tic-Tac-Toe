9f29c0e65c6df2f15e7c2fc541cc2725cbf0072b
erengorgun-nkit <115351981+erengorgun-nkit@users.noreply.github.com>
2026-07-08 11:53:09 +0300

    cell id fix, global variables

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
 const msgBox = document.querySelector("#msgbox");
 
-function createPlayer(event) {
-  const name1 = document.querySelector("#p1name").value;
-  const name2 = document.querySelector("#p2name").value;
-  const p1Marker = document.querySelector('input[name="p1"]:checked')?.value;
-  const p2Marker = document.querySelector('input[name="p2"]:checked')?.value;
-  if (name1.trim() != "" && name2.trim() != "" && p1Marker !== p2Marker) {
-    return {
-      player1: {
-        name: name1,
-        marker: p1Marker,
-        moveList: [],
-        score: 0,
-      },
-      player2: {
-        name: name2,
-        marker: p2Marker,
-        moveList: [],
-        score: 0,
-      },
-    };
-  } else {
-    msgBox.textContent = "Enter in the requested values";
-    event.preventDefault();
-    return false;
-  }
-}
-
 const gameboard = (() => {
   const board = ["", "", "", "", "", "", "", "", ""];
   const getBoard = () => board;
+
   const placeMarker = (target, marker) => {
     if (target != "") {
       if (marker == "X") board[target - 1] = "X";
@@ -39,33 +13,86 @@ const gameboard = (() => {
   return { getBoard, placeMarker };
 })();
 
-function renderBoard() {
-  const main = document.querySelector("main");
-  main.innerHTML = "";
+function displayController() {
+  const renderBoard = () => {
+    const board = gameboard.getBoard();
+    const main = document.querySelector("main");
+    const gbContainer = document.createElement("div");
 
-  const board = gameboard.getBoard();
-  const gbContainer = document.createElement("div");
-  gbContainer.classList.add("board");
-  main.appendChild(gbContainer);
+    main.innerHTML = "";
+    gbContainer.classList.add("board");
+    main.appendChild(gbContainer);
 
-  for (let i = 0; i < board.length; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.setAttribute("id", board[i]);
-    gbContainer.appendChild(cell);
-  }
+    for (let i = 0; i < board.length; i++) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.id = `${i + 1}`;
+      gbContainer.appendChild(cell);
+    }
+  };
+
+  const placeMarker = (target, marker) => {
+    if (target.textContent === "") target.textContent = marker;
+  };
+
+  return { renderBoard, placeMarker };
 }
 
-function gameController(event) {
-  const players = createPlayer(event);
-  if (players) {
-    let p1 = createPlayer().player1;
-    let p2 = createPlayer().player2;
-    console.log(players);
-    renderBoard();
-  }
+function gameController() {
+  let inputtedCell, currentPlayer;
+  const createPlayer = (event) => {
+    const name1 = document.querySelector("#p1name").value,
+      name2 = document.querySelector("#p2name").value,
+      p1Marker = document.querySelector('input[name="p1"]:checked')?.value,
+      p2Marker = document.querySelector('input[name="p2"]:checked')?.value;
+    if (name1.trim() != "" && name2.trim() != "" && p1Marker !== p2Marker) {
+      return {
+        player1: {
+          name: name1,
+          marker: p1Marker,
+          moveList: [],
+          score: 0,
+        },
+        player2: {
+          name: name2,
+          marker: p2Marker,
+          moveList: [],
+          score: 0,
+        },
+      };
+    } else {
+      msgBox.textContent = "Enter in the requested values";
+      event.preventDefault();
+      return false;
+    }
+  };
+
+  document.querySelector("main").addEventListener("click", (e) => {
+    if (e.target.classList.contains("cell")) {
+      inputtedCell = e.target.id;
+      displayController().placeMarker(e.target, currentPlayer.marker);
+    }
+  });
+  const playerTurn = (player) => {
+    msgBox.textContent = `Your turn, ${player.name} (${player.marker})`;
+  };
+
+  const newGame = (event) => {
+    const players = createPlayer(event);
+    if (players) {
+      let p1 = players.player1,
+        p2 = players.player2,
+        turnCount = 0;
+      console.log(p1, p2);
+      displayController().renderBoard();
+      currentPlayer = p1;
+      playerTurn(currentPlayer);
+    }
+  };
+
+  return { createPlayer, newGame };
 }
 
 document.querySelector("#btnstart").addEventListener("click", (e) => {
-  gameController(e);
+  gameController().newGame(e);
 });
